0fac647f2aad3ece812709b0e1d7e13d32d8886f
ytyaru <ytyaru@outlook.jp>
2019-04-06 12:54:08 +0900

    bashの特殊パラメータ

---
+++ src/05_special_parameter.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#set -Ceu
+#-----------------------------------------------------------------------------
+# bashの特殊パラメータ
+# 作成日時: 2019-04-06T12:49:52+0900
+# http://ytyaru.hatenablog.com/entry/2020/06/15/000000
+#-----------------------------------------------------------------------------
+Here() { echo "$0"; }
+Filename() { echo "$(basename "$0")"; }
+DirPath() { echo $(cd $(dirname "$0"); pwd); }
+FullPath() { echo "$(cd $(dirname "$0"); pwd)/$(Filename)"; }
+ShowParams() {
+	echo "ParamNum: $#"
+	echo "Params: '$*'"
+	local count=1
+	for p in $@; do echo "$count: $p"; ((count++)); done;
+}
+Result() {
+	[[ 1 -eq 1 ]] && echo "True: $?" || echo "False: $?"
+	[[ 1 -ne 1 ]] && echo "True: $?" || echo "False: $?"
+}
+Run() {
+	# bash実行時のフラグ
+	echo "$-"
+	echo "$BASHPID"
+	# シェルのPID
+	echo "$$"
+	# 直前に実行したバックグラウンドコマンドのPID
+	echo "$!"
+	Here
+	Filename
+	DirPath
+	FullPath
+	ShowParams A B C
+	Result
+}
+Run
